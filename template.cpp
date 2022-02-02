4e0a8b338666080170967bf7b55064f7f368ef43
Chintan Suthar <75067919+chintansuthar72@users.noreply.github.com>
2022-02-02 10:58:16 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,76 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <string.h>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
+#include <iostream>
+using namespace std;
+using ll = long long int;
+using lld = long double;
+const long long INF = 1e18;
+const int32_t MOD = 1e9+7;
+const int N = 1e5 + 3;
+#define bitc(n) __builtin_popcount(n)
+#define bitcll(n) __builtin_popcountll(n)
+#define endl "\n"
+#define loop(i,a,b) for(int i=a; i<=b; ++i)
+#define vi vector<int>
+#define vll vector<long long>
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define ppb  pop_back
+#define pf  push_front
+#define ppf  pop_front
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound 
+#define all(x) (x).begin(),(x).end()
+#define pii pair<int,int>
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+
+// bitset<MAX_N> b --> array of bits
+
+// ceil of (a/b)
+// ll divide(ll a,ll b){
+//     return (a+b-1)/b;
+// }
+
+// ll gcd(ll a,ll b){
+//  	while(a){
+//  		ll r = b%a;
+//  		b = a;
+//  		a = r;
+//  	}
+//  	return b;
+// }
+
+void test_case(){
+	
+}
+
+int32_t main(){
+	//#ifndef CHINTAN
+    //freopen("in.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
+    //#endif
+	ll tc = 1;
+	cin >> tc;
+	for(ll T=0; T<tc; ++T){
+		test_case();
+	}
+    return 0;
+}
